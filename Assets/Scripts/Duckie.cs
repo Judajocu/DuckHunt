3fd8372090936b39432402f8a0aeae5b1df0ddaf
Judajocu <Juandanieljoa@gmail.com>
2016-10-19 00:30:56 -0400

    primer commit

---
+++ Assets/Scripts/Duckie.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Duckie : MonoBehaviour {
+
+    #region Enums:
+
+        enum Direction
+    {
+        north, south, este, oeste
+    }
+    #endregion
+    Direction direc;
+    // Use this for initialization
+    void Start () {
+        //gameObject.SetActive(false);
+        InvokeRepeating("MovDuck", 1f,6f);
+        LeavingGrass();
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        
+	}
+
+    void LeavingGrass()
+    {
+
+    }
+
+    void DyingDuck()
+    {
+
+    }
+
+    void MovDuck()
+    {
+        //gameObject.SetActive(true);
+        float randX = Random.Range(-3f, 3f);
+        float randY = Random.Range(-1f, 4.5f);
+        gameObject.transform.position = new Vector3(randX, randY);
+        gameObject.transform.Translate(gameObject.transform.position.x, gameObject.transform.position.y,0);
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    GameObject Puntero;
+    GameObject Player2;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+       // Puntero.transform.position = Input.mousePosition;
+        //Puntero.transform.Translate(new Vector3(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y")));
+
+    }
+}
